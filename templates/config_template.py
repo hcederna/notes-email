3da28baf7738d38510ea6c1b99ec3fd60c72f96d
Hillary Cederna <hcederna@gmail.com>
2020-01-29 10:54:50 -0800

    Create templates directory with config and learning notes templates

---
+++ templates/config_template.py
@@ -0,0 +1,10 @@
+# send notes digest from this email address
+# find unread kindle book highlights in this account inbox
+GMAIL_USER = "your_email@gmail.com"
+GMAIL_PASSWORD = "your_app_password"
+
+# send notes digest to this email address
+RECIPIENT_EMAIL = "specified_recipient_email@gmail.com"
+
+# send this number of random learning notes in notes digest email
+NUM_NOTES = 5

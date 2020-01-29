5a0dfd22bcc7cf1449e80a53cc17cc9ab51bb1a1
Hillary Cederna <hcederna@gmail.com>
2020-01-29 10:54:15 -0800

    Clean up files for upload to GitHub

--- notes_email.py
+++ notes_email.py
@@ -15,17 +15,22 @@ from src.format_learning_notes import format_learning_notes
 from src.send_email import send_email
 
 from config import GMAIL_USER, GMAIL_PASSWORD
-from config import KINDLE_RAW_NOTES_DIRECTORY, KINDLE_FORMATTED_NOTES_DIRECTORY, KINDLE_RAW_NOTES_ARCHIVE_DIRECTORY
-from config import APPLE_NOTES_RAW_LEARNING_NOTES_DIRECTORY, APPLE_NOTES_FORMATTED_NOTES_DIRECTORY
 from config import RECIPIENT_EMAIL, NUM_NOTES
 
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+KINDLE_RAW_NOTES_DIRECTORY = dir_path + "/data/kindle/raw"
+KINDLE_FORMATTED_NOTES_DIRECTORY = dir_path + "/data/kindle/"
+KINDLE_RAW_NOTES_ARCHIVE_DIRECTORY = dir_path + "/data/kindle/raw/archive"
+APPLE_NOTES_RAW_LEARNING_NOTES_DIRECTORY = dir_path + "/data/apple_notes/raw"
+APPLE_NOTES_FORMATTED_NOTES_DIRECTORY = dir_path + "/data/apple_notes"
 
-import_kindle_notes(GMAIL_USER, GMAIL_PASSWORD, KINDLE_RAW_NOTES_ARCHIVE_DIRECTORY)
+
+import_kindle_notes(GMAIL_USER, GMAIL_PASSWORD, KINDLE_RAW_NOTES_DIRECTORY)
 format_kindle_notes(KINDLE_RAW_NOTES_DIRECTORY, KINDLE_FORMATTED_NOTES_DIRECTORY, KINDLE_RAW_NOTES_ARCHIVE_DIRECTORY)
 
 # run import learning notes applescript
-os.system("osascript /Users/hcederna/Desktop/notes-email/src/import_learning_notes.scpt")
+os.system("osascript " + dir_path + "/src/import_learning_notes.scpt")
 format_learning_notes(APPLE_NOTES_RAW_LEARNING_NOTES_DIRECTORY, APPLE_NOTES_FORMATTED_NOTES_DIRECTORY)
 
 send_email(GMAIL_USER, GMAIL_PASSWORD, RECIPIENT_EMAIL, NUM_NOTES, KINDLE_FORMATTED_NOTES_DIRECTORY, APPLE_NOTES_FORMATTED_NOTES_DIRECTORY)
